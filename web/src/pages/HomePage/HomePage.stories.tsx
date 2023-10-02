d26a347e8c098d6144700a2b08fef3c2ba82b9e4
Faddah Wolf <my_biz@me.com>
2023-10-02 08:38:14 -0700

    Re-did import of HomePage for Storybook.

--- web/src/pages/HomePage/HomePage.stories.tsx
+++ web/src/pages/HomePage/HomePage.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import HomePage from './Old-HomePage'
+import HomePage from './HomePage'
 
 const meta: Meta<typeof HomePage> = {
 	component: HomePage,
