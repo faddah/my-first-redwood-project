cf80a5fd0977798762a070a6f63e5bf550748237
Faddah Wolf <my_biz@me.com>
2023-10-02 08:39:23 -0700

    Add code for SignUp dialogue on HomPage.tsx.

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,5 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
 import { useAuth } from 'src/auth'
 
 const HomePage = () => {
@@ -5,10 +7,20 @@ const HomePage = () => {
 
 	return (
 		<>
-			{/* MetaTags, h1, paragraphs, etc. */}
+			<MetaTags title="Home" description="Home page" />
 
+			<h1>HomePage</h1>
+			<p>
+				Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
+			</p>
+			<p>
+				My default route is named <code>home</code>, link to me with `
+				<Link to={routes.home()}>Home</Link>`
+			</p>
 			<p>{JSON.stringify({ isAuthenticated })}</p>
 			<button onClick={signUp}>sign up</button>
 		</>
 	)
 }
+
+export default HomePage
